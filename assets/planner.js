9e08bb66545ef53bea4f51bf33286e746e7cc12d
James Gould <nobleburgundy@gmail.com>
2020-10-26 09:45:02 -0500

    Fixed issue with currentTime comparison.

--- assets/planner.js
+++ assets/planner.js
@@ -3,32 +3,34 @@ $(document).ready(function () {
 
   // Show current date and current time
   setInterval(() => {
-    $("#current-date").text(moment().format("MMMM Do YYYY, h:mm a"));
+    $("#current-date").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
   }, 1000);
 });
 
 let workHoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 let currentTime = moment().format("H");
+// let currentTime = 6; // just used for testing
 
 function setRowClassesBasedOnTime() {
   let pastHoursClass = "table-secondary";
   let currentHourClass = "table-danger";
   let futureHourEmptyClass = "table-success";
   let futureHourFilledClass = "table-info";
+  // For comparison
+  let currentTimeInt = parseInt(currentTime);
 
   for (let i = 0; i < workHoursArray.length; i++) {
-    if (workHoursArray[i] < currentTime) {
+    if (workHoursArray[i] < currentTimeInt) {
       $(`tr#${workHoursArray[i]}`).addClass(pastHoursClass);
-    } else if (workHoursArray[i] === currentTime) {
+    } else if (workHoursArray[i] === currentTimeInt) {
       $(`tr#${workHoursArray[i]}`).addClass(currentHourClass);
     } else {
       // Future hours, now check if there is a meeting
       let tdElement = $(`tr#${workHoursArray[i]} td:nth-child(2)`);
-      console.log(tdElement.text());
-      //   let tdElement = trElement.children().;
-      if (tdElement) {
+      if (tdElement.text()) {
         $(`tr#${workHoursArray[i]}`).addClass(futureHourFilledClass);
       } else {
+        console.log("empty");
         $(`tr#${workHoursArray[i]}`).addClass(futureHourEmptyClass);
       }
     }
