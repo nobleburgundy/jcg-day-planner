b293fbf2198a9201b4c7d1c198e4473e600e0524
James Gould <nobleburgundy@gmail.com>
2020-10-29 22:42:50 -0500

    Added logic to delete meeting. Also more logic around show/hide icons.

--- assets/planner.js
+++ assets/planner.js
@@ -61,7 +61,12 @@ $(".fa-save").on("click", function () {
   }
   // update the object
   meetingDataArray[timeBlockInt - meetingDataArray.length].description = cellContents;
-  console.log(meetingDataArray);
+  // update classes
+  $(this).parent().parent().addClass("filled").removeClass("empty");
+  // Hide icon
+  $(this).addClass("d-none");
+  // Show trash icon
+  $(this).nextAll(".fa-trash").removeClass("d-none");
   // save to localStorage
   saveMeetingsToLocalStorage();
 });
@@ -81,6 +86,25 @@ function loadMeetingsFromLocalStorage() {
     // update the meetinDataArray with the locally stored data
     meetingDataArray = localMeetingData;
   }
+
+  $(document).on("click", ".fa-trash", function () {
+    // Remove this item's parents sibling
+    let timeBlock = $(this).parent().prevAll(".time").text();
+    let timeBlockInt = parseInt(timeBlock);
+    // convert to 24 time
+    if (timeBlockInt < 9) {
+      timeBlockInt += 12;
+    }
+    // update the object
+    meetingDataArray[timeBlockInt - meetingDataArray.length].description = "";
+    // clear the field
+    $(this).parent().prevAll(".meeting-description").text("");
+    $(this).parent().parent().addClass("empty").removeClass("filled");
+    $(this).addClass("d-none");
+    // $(this).parent().parent().addClass("empty");
+    // update localStorage
+    saveMeetingsToLocalStorage();
+  });
 
   // write data to the page
   for (let index = 0; index < localMeetingData.length; index++) {
